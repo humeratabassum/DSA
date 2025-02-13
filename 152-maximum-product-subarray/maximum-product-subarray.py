d69c20e4679cf559b565413e346c3b85bf846d92
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-02-13 14:45:32 +0530

    Time: 15 ms (18.51%) | Memory: 13.2 MB (40.54%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        if not nums:
+            return 0
+        max_product=nums[0]
+        min_product=nums[0]
+        result=nums[0]   
+        
+        for i in range(1,len(nums)):
+            if nums[i]<0:   #swap when encounter a neg number
+                max_product,min_product=min_product,max_product
+            max_product=max(nums[i],nums[i]*max_product)
+            min_product=min(nums[i],nums[i]*min_product)
+            result=max(result,max_product)
+        return result
+
+
+
+
+        
