6e68389a74b29b978c50084d5d17e7adef7bf27a
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-02-17 15:57:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (62.02%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,23 +1,40 @@
+# class Solution(object):
+#     def intersect(self, nums1, nums2):
+#         nums1.sort()
+#         nums2.sort()
+#         i,j=0,0
+#         result=[]
+
+#         while i < len(nums1) and j<len(nums2):
+#             if nums1[i]==nums2[j]:
+#                 result.append(nums1[i])
+#                 i+=1
+#                 j+=1
+#             elif nums1[i]<nums2[j]:
+#                 i+=1
+#             else:
+#                 j+=1
+#         return result
+#         """
+#         :type nums1: List[int]
+#         :type nums2: List[int]
+#         :rtype: List[int]
+#         """
+        
 class Solution(object):
     def intersect(self, nums1, nums2):
         nums1.sort()
         nums2.sort()
-        i,j=0,0
-        result=[]
-
-        while i < len(nums1) and j<len(nums2):
-            if nums1[i]==nums2[j]:
-                result.append(nums1[i])
-                i+=1
-                j+=1
-            elif nums1[i]<nums2[j]:
-                i+=1
+        ans = []
+        i, j = 0, 0
+        n, m = len(nums1), len(nums2)
+        while i < n and j < m:
+            if nums1[i] == nums2[j]:
+                ans.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
             else:
-                j+=1
-        return result
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        
+                j += 1
+        return ans
