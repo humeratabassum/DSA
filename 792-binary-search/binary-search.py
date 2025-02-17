0dc3c83131b29a54d06e5d2662975366f7e93c08
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-02-17 16:20:24 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (32.00%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,12 +1,18 @@
 class Solution(object):
     def search(self, nums, target):
-        for i in range(len(nums)):
-            if nums[i]==target:
-                return i
+        l,r=0,len(nums)-1
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                l=mid+1
+            else:
+                r=mid-1
         return -1
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
         
+
+        # for i in range(len(nums)):
+        #     if nums[i]==target:
+        #         return i
+        # return -1
