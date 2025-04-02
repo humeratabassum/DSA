fd5cc596cbeaf16878bcfc4d76c9385425e73a3c
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-04-02 22:59:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (12.18%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        l,r=0,len(nums)-1
+
+        while l<r:
+            mid=(l+r)//2
+
+            if nums[mid]>nums[mid+1]:
+                r=mid
+            else:
+                l=mid+1
+        return l
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
