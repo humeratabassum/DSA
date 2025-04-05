6b7cf1c35baf568219e709a6a8a5e804b1364995
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-04-05 10:28:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+class Solution:
+    def findKRotation(self, arr):
+        return arr.index(min(arr))
+        # code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input().strip())
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        res = ob.findKRotation(arr)
+        print(res)
+        print("~")
+# } Driver Code Ends
