c073c657cfc766529869cfd17b08a3b6efa0f3d6
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-06-05 21:51:22 +0530

    Time: 2 ms (18.48%) | Memory: 19.4 MB (29.66%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -1,31 +1,20 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-# class Solution:
-#     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
 
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
             return None
+        
+        arr=[]
+        while head:
+            arr.append(head.val)
+            head=head.next   #
+        
+        reordered=arr[::2]+arr[1::2]
 
-        # Collect values in order
-        vals = []
-        temp = head
-        while temp:
-            vals.append(temp.val)
-            temp = temp.next
-
-        # Reorder values by odd index first, then even
-        reordered = vals[::2] + vals[1::2]
-
-        # Create a new linked list
-        dummy = ListNode(0)
-        curr = dummy
-        for val in reordered:
-            curr.next = ListNode(val)
-            curr = curr.next
-
-        return dummy.next
+        new=ListNode(0)
+        curr=new
+        for i in reordered:
+            curr.next=ListNode(i)
+            curr=curr.next
+        return new.next
