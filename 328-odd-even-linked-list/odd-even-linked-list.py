7c018119d4eeea8301db1ddd88fc6b14865e88a2
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-06-05 22:15:09 +0530

    Time: 4 ms (5.78%) | Memory: 19.2 MB (45.98%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -1,5 +1,30 @@
+#solution using 2 pointers odd and even
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        odd,even=head, head.next
+        even_ptr=even
+
+        while even and even.next:
+            odd.next=odd.next.next
+            even.next=even.next.next
+        odd.next=even_ptr
+        return head
 
 
+
+
+
+
+
+
+
+
+
+
+
+# this solution is using arrays 
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
