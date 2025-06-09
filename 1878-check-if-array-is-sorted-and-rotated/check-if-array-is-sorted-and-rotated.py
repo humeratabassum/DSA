fddd07cde1646eba716f79d2bc416459374d506f
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-06-09 23:07:00 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.63%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -6,7 +6,11 @@ class Solution:
         for i in range(n):
             if nums[i]>nums[(i+1)%n]:
                 count+=1
-        return count<=1
+            
+        if count<=1:
+            return True
+        else:
+            return False
 
 
         
