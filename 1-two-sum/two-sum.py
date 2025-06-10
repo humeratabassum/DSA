7b4c7f2a2d2c1c59b39f0c397c286e7bfd4ef49c
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-06-11 01:03:27 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (33.99%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,3 +1,4 @@
+"""          BRUTE FORCE
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
@@ -5,4 +6,15 @@ class Solution:
                 if nums[i]+nums[j]==target:
                     return [i,j]
         return False
-        
+"""
+"""OPTIMAL  Solution using hasmap / dictionary"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h_map={}    #dictionary (numbers:indices)
+        for i,num in enumerate(nums):
+            diff=target-num
+            if diff in h_map:
+                return [i,h_map[diff]]
+            h_map[num]=i   #add current number and value in h_map if it doesnt exist
+        return False
+
