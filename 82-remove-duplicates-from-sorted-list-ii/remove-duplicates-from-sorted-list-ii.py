f45dd31e23b4738f285ce1c152edbfae67668652
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-06-13 01:43:05 +0530

    Time: 1 ms (39.61%) | Memory: 17.8 MB (43.26%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -3,23 +3,37 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy=ListNode(0,head)
         prev=dummy
-        current=head
-        while current and current.next:
-            if current.val == current.next.val:
-                while current.next and current and current.val==current.next.val:
+        curr=head
 
-                    current=current.next
-                    
-                prev.next=current.next
+        while curr and curr.next:
+            if curr.val==curr.next.val:
+                while curr and curr.next and curr.val==curr.next.val:
+                    curr=curr.next
+                prev.next=curr.next
+                
             else:
                 prev=prev.next
-            current=current.next
+            curr=curr.next
         return dummy.next
 
-        
+"""
+curr=curr.next
+Keeps moving curr forward as long as the same value repeats.
+Helps skip all duplicate nodes in a group (like 3 → 3 → 3).
 
-        
+
+prev.next = curr.next
+Cuts out the whole duplicate group.
+Links the last unique node (prev) to the next new value after duplicates.
+
+
+"""
+
+       
