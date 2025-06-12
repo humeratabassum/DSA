b75f0a372b1248111d2ef0c9aeeafd9dc279e72d
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-06-12 17:25:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (61.23%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,26 +1,19 @@
-# # Definition for singly-linked list.
-# # class ListNode(object):
-# #     def __init__(self, val=0, next=None):
-# #         self.val = val
-# #         self.next = next
-# class Solution(object):
-#     def mergeTwoLists(self, list1, list2):
-#         return sorted(list1+list2)
-#         """
-#         :type list1: Optional[ListNode]
-#         :type list2: Optional[ListNode]
-#         :rtype: Optional[ListNode]
-#         """
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1, list2):
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if not list1:
             return list2
         if not list2:
             return list1
-
-        if list1.val < list2.val:
-            list1.next = self.mergeTwoLists(list1.next, list2)
+        if list1.val<list2.val:
+            list1.next=self.mergeTwoLists(list1.next,list2)
             return list1
         else:
-            list2.next = self.mergeTwoLists(list1, list2.next)
+            list2.next=self.mergeTwoLists(list1,list2.next)
             return list2
+
+        
