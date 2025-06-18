43046edc3719d4199c71f1fd7a707098f220425d
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-06-19 01:20:01 +0530

    Time: 7 ms (58.64%) | Memory: 20.6 MB (50.57%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,23 +1,39 @@
-# class Solution:
-#     def majorityElement(self, nums: List[int]) -> List[int]:
-#         if not nums:
-#             return []
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
         
-#         #first find 2 candidates
-#         count1,count2=0,0
-#         cand1=cand2=None
+        #first find 2 candidates
+        count1,count2=0,0
+        cand1=cand2=None
 
-#         for num in nums:
-#             if num==cand1:
-#                 count1+=1
-#             elif num==cand2:
-#                 count2+=1
-#             elif count1==0:
-#                 cand1,count1=num,1
-#             elif count2==0:
-#                 cand2,count2=num,1
-#             else:
-#                 count1-=1
+        for num in nums:
+            if num==cand1:
+                count1+=1
+            elif num==cand2:
+                count2+=1
+            elif count1==0:
+                cand1,count1=num,1
+            elif count2==0:
+                cand2,count2=num,1
+            else:
+                count1-=1
+                count2-=1
+        result=[]
+        n=len(nums)
+        if nums.count(cand1) > n // 3:
+            result.append(cand1)
+        if cand2 != cand1 and nums.count(cand2) > n // 3:
+            result.append(cand2)
+        
+        return result
+
+
+
+
+
+#solution 2
+"""
 class Solution:
     def majorityElement(self, nums: list[int]) -> list[int]:
         # Create a Counter to store the count of each element
@@ -34,4 +50,4 @@ class Solution:
         
         return majority_elements
 
-        
+"""
