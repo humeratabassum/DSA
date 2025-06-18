6f5f6dcb6e0a2099b74df45236495f4e45930baa
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-06-18 23:29:48 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (31.87%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,6 +1,13 @@
-class Solution(object):
-    def findMin(self, nums):
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        #irst find mid of the array to split it
         l,r=0,len(nums)-1
+# compare the mid with right pointer
+# 3, 4, 5, 1, 2
+# l     m     r   
+ #here mid>right , max elements are on left side so we place pointer towards the right 
+#else right will move to mid eg:[11,12,13,15,17]
+
         while l<r:
             mid=(l+r)//2
             if nums[mid]>nums[r]:
@@ -8,10 +15,4 @@ class Solution(object):
             else:
                 r=mid
         return nums[l]
-
-
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         
