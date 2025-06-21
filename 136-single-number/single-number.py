b68129eaea547e5759422999b6d545ce4c6d8a27
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-06-21 23:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (44.04%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -2,6 +2,6 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         result=0
         for num in nums:
-            result=result^num
+            result=result^num   #xor operation 
         return result
         
