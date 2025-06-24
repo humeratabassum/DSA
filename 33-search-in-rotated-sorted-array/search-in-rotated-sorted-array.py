218bbabbbab4dbec896eb907f5f3b309a87cd151
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-06-24 23:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (22.08%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,74 +1,27 @@
-# # class Solution(object):
-# #     def search(self, nums, target):
-# #         low, high = 0, len(nums) - 1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l,r=0,len(nums)-1
+        
 
-# #         while low <= high:
-# #             mid = (low + high) // 2
+        while l<=r:
+            mid=(l+r)//2
 
-# #             if nums[mid] == target:
-# #                 return True 
-# #                  # Target found
-# #             if nums[low]<=nums[mid]:
-# #                 if nums[low]<=target<nums[mid]:
-# #                     high=mid-1
-# #                 else:
-# #                     low=mid+1
-# #             else:
-# #                 if nums[mid]<target<nums[high]:
-# #                     low=mid+1
-# #                 else:
-# #                     high=mid-1
-# #         return False
-
-class Solution(object):
-    def search(self, nums, target):
-        low, high = 0, len(nums) - 1
-
-        while low <= high:
-            mid = (low + high) // 2
-
-            if nums[mid] == target:
-                return mid  # Or return mid if index is required
-
-            # Left part is sorted
-            if nums[low] <= nums[mid]:
-                if nums[low] <= target < nums[mid]:
-                    high = mid - 1
+            if nums[mid]==target:
+                return mid
+            
+            if nums[l]<=nums[mid]:
+                if nums[l]<=target<nums[mid]:
+                    r=mid-1
                 else:
-                    low = mid + 1
-
-            # Right part is sorted
+                    l=mid+1
             else:
-                if nums[mid] < target <= nums[high]:
-                    low = mid + 1
+                if nums[mid]<target<=nums[r]:
+                    l=mid+1
                 else:
-                    high = mid - 1
-
+                    r=mid-1
         return -1
 
-# class Solution(object):
-#     def search(self, nums, target):
-#         left, right = 0, len(nums) - 1
 
-#         while left <= right:
-#             mid = (left + right) // 2
 
-#             if nums[mid] == target:
-#                 return mid  # Target found, return index
 
-#             # Left half is sorted
-#             if nums[left] <= nums[mid]:
-#                 if nums[left] <= target < nums[mid]:
-#                     right = mid - 1  # Target in left
-#                 else:
-#                     left = mid + 1   # Target in right
-
-#             # Right half is sorted
-#             else:
-#                 if nums[mid] < target <= nums[right]:
-#                     left = mid + 1   # Target in right
-#                 else:
-#                     right = mid - 1  # Target in left
-
-#         return -1  # Not found
-      
+        
