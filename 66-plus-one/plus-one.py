101d8c51fbdfc8873bbf558df4a5d95a4b2a2827
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-06-25 00:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (8.79%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -5,7 +5,9 @@ class Solution:
             if digits[i]<9:
                 digits[i]+=1
                 return digits
-            digits[i]=0
+            else:
+                digits[i]=0
+                i-=1
         return [1]+digits
 
 
