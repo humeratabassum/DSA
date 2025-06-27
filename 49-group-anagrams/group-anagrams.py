11206c9e43a1c17e61e4bef3ecd1dc8f2a8ba7d4
humeratabassum <130923515+humeratabassum@users.noreply.github.com>
2025-06-27 23:31:10 +0530

    Time: 9 ms (90.27%) | Memory: 20.7 MB (76.73%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,32 @@
+#default dict is used to store group of anagrams
+#defaultdict(list) automatically creates a list when a new key is used 
+
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map=defaultdict(list)
+
+        for word in strs:
+            key=''.join(sorted(word))
+            anagram_map[key].append(word)
+        return list(anagram_map.values())
+
+
+""" it is in the form of key value pairs
+{
+  "aet": ["eat", "tea", "ate"],
+  "ant": ["tan", "nat"],
+  "abt": ["bat"]
+}
+"""
+"""
+| Word | Sorted Letters   | Key   |
+| ---- | ---------------- | ----- |
+| eat  | \['a', 'e', 't'] | "aet" |
+| tea  | \['a', 'e', 't'] | "aet" |
+| tan  | \['a', 'n', 't'] | "ant" |
+| ate  | \['a', 'e', 't'] | "aet" |
+| nat  | \['a', 'n', 't'] | "ant" |
+| bat  | \['a', 'b', 't'] | "abt" |
+"""
