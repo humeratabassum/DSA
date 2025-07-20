3c7922d1c2dfc7e3ee67727c0cc3d28447989e3a
Humera Tabassum <130923515+humeratabassum@users.noreply.github.com>
2025-07-20 18:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.11%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -1,11 +1,22 @@
+# optimal solution using bit manipukatiom
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
+        return n>0 and (n&(n-1)==0)
+
+
+
+
+
+
+
+
+#bruteforce approach
+"""
         if n<=0:
             return False
         while n!=1:
             if n%2!=0:
                 return False
             n=n//2
-        
         return True
-        
+"""
