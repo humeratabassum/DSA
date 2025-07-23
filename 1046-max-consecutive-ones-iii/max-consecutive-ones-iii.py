ba643001289010fbf41472e45a64230cdb37b56d
Humera Tabassum <130923515+humeratabassum@users.noreply.github.com>
2025-07-23 22:26:33 +0530

    Time: 31 ms (91.64%) | Memory: 20.3 MB (15.03%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -4,11 +4,11 @@ class Solution:
         for r in range(len(nums)):
             if nums[r]==0:
                 k-=1    #use a flip
-            if k<0:
+            if k<0:            #if available k are less then we shrink using left pointer
                 if nums[l]==0:
-                    k+=1
-                l+=1
-        return r-l+1
+                    k+=1            #if l==0 , update k . add 1
+                l+=1   #
+        return r-l+1        #this returns max window l-r+1
 
             
         
