5cdea2c4a7bc8775e0c29933b99b6c5962924c4b
Humera Tabassum <130923515+humeratabassum@users.noreply.github.com>
2025-07-24 23:56:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (52.26%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,14 +1,22 @@
-class Solution(object):
-    def removeElement(self, nums, val):
+"""
+How It Works (Step-by-Step):
+k keeps track of the next position to place a value that is NOT equal to val.
+
+Loop through each element:
+
+If nums[i] != val, copy it to nums[k], and move k forward.
+
+
+
+"""
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
         k=0
         for i in range(len(nums)):
             if nums[i]!=val:
                 nums[k]=nums[i]
                 k+=1
         return k
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
         
