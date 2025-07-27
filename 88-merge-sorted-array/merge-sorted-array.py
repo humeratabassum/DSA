76c4e749d292c82aa1a80515991ed31d5fa879d6
Humera Tabassum <130923515+humeratabassum@users.noreply.github.com>
2025-07-27 22:45:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (71.76%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,8 +1,30 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
+        for j in range(n):
+            nums1[m+j]=nums2[j]
+        nums1.sort()
+""" APPROACH
+You are given two sorted arrays:
+
+nums1 of size m + n, where the first m elements are valid and the rest (n zeros) are placeholders.
+
+nums2 of size n.
+
+
+Your task is to merge nums2 into nums1 as one sorted array in-place.
+
+
+"""
+
+
+
+
+
+
+
+"""
         Do not return anything, modify nums1 in-place instead.
-        """
+"""        """
         i=m-1
         j=n-1
         k=m+n-1
@@ -19,4 +41,4 @@ class Solution:
             nums1[k]=nums2[j]
             j-=1
             k-=1
-        
+"""
