d79fd21eae60d1d889b989921152a4e1a5c9098a
Humera Tabassum <130923515+humeratabassum@users.noreply.github.com>
2025-07-30 23:41:12 +0530

    Time: 0 ms (100%), Space: 17.9 MB (32.25%) - LeetHub

---
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        count=0
+        res=0
+        for i in s:
+            if i=='R':
+                count+=1
+            else:
+                count-=1
+            if count==0:
+                res+=1
+        return res
+                
