d79a26323a4b9336a19e7239ec6ea36cbe24d9ca
Humera Tabassum <130923515+humeratabassum@users.noreply.github.com>
2025-07-31 23:01:36 +0530

    Time: 11 ms (34.86%), Space: 17.9 MB (83.17%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #create an empty string to store only letters and numbers
+         #alphanumeric means letters or numbers , no symbols
+
+        cleaned_string=''
+        for char in s:
+            if char.isalnum():
+                cleaned_string+=char.lower()
+        
+        if cleaned_string==cleaned_string[::-1]:
+            return True
+        else:
+            return False
+            
