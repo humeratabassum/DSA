dedff40de7f92397545f7706f19f5d2f3d5d185c
Humera Tabassum <130923515+humeratabassum@users.noreply.github.com>
2025-08-16 16:08:15 +0530

    Time: 149 ms (20.46%), Space: 22 MB (60.18%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
@@ -3,6 +3,12 @@ class Solution:
         count=0
         n=len(grid)
 
+# In Python, grids are usually lists of rows, but sometimes you want the columns too!
+
+# This line creates a list called cols, where each item is a column from the grid.
+
+
+
         cols=[[grid[row][col] for row in range(n)] for col in range(n)]
 
         for row in grid:
