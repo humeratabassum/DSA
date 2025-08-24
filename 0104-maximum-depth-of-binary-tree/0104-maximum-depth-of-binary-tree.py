52ac936b2c7d4d2ee2395e47b0442a2013fb7d2c
Humera Tabassum <130923515+humeratabassum@users.noreply.github.com>
2025-08-24 20:09:47 +0530

    Time: 0 ms (100%), Space: 19.3 MB (7.43%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -9,5 +9,10 @@ class Solution:
         if not root:
             return 0
         
-        return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))
+        left=self.maxDepth(root.left)
+        right=self.maxDepth(root.right)
+
+        return 1+max(left,right)
+
+        # return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))
         
