b35128cb1648bbf32af322b32e240e1b4c63c637
Humera Tabassum <130923515+humeratabassum@users.noreply.github.com>
2025-08-27 12:59:52 +0530

    Time: 3 ms (78.89%), Space: 21.1 MB (98.49%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -17,7 +17,6 @@ class Solution:
         def inorder(node):
             if not node:
                 return
-            
             inorder(node.left)
 
             if self.prev==node.val:
@@ -32,10 +31,43 @@ class Solution:
             
             elif self.count==self.max:
                 self.mode.append(node.val)
-            
+
             inorder(node.right)
         inorder(root)
         return self.mode
+
+
+
+
+
+
+        # self.prev=None
+        # self.count=0
+        # self.max=0
+        # self.mode=[]
+
+        # def inorder(node):
+        #     if not node:
+        #         return
+            
+        #     inorder(node.left)
+
+        #     if self.prev==node.val:
+        #         self.count+=1
+        #     else:
+        #         self.count=1
+        #         self.prev=node.val
+            
+        #     if self.count>self.max:
+        #         self.max=self.count
+        #         self.mode=[node.val]
+            
+        #     elif self.count==self.max:
+        #         self.mode.append(node.val)
+            
+        #     inorder(node.right)
+        # inorder(root)
+        # return self.mode
 
 
 
