b45c751d094896461348154b5a317460a1c190ef
Humera Tabassum <130923515+humeratabassum@users.noreply.github.com>
2025-09-17 21:06:55 +0530

    Time: 733 ms (44.58%), Space: 0B (100%) - LeetHub

--- 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
@@ -5,6 +5,3 @@ SELECT *
 FROM Users
 WHERE REGEXP_LIKE(mail, '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\\.com$', 'c');
 
--- SELECT *
--- FROM Users
--- WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode(\\.co)?\\.com$';
